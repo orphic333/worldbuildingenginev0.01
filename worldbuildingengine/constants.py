f9fdfd567bccd8c258b98362c2f3d4bb9354a9f3
Kwaku Duodu Baidoo <baidookwaku20@gmail.com>
2026-05-29 22:09:48 +0000

    added resources to resource class. Defined hero specialisations and their descriptions.

--- worldbuildingengine/constants.py
+++ worldbuildingengine/constants.py
@@ -46,21 +46,44 @@ ZONE_MODIFIERS = [
 ]
 
 class Resource(enum.Enum):
-    RAW_AETHER = "raw_aether"
-    OBSIDIAN = "obsidian"
-    BONE = "bone"
-    SHADOW_MATTER = "shadow_matter"
+    AETHER_CRYSTALS = "aether_crystals"
+    AETHERITE = "aetherite"
+    BLOOD = "blood"
+    CATTLE = "cattle"
+    COPPER = "copper"
+    CREATURES = "creatures"
+    FIBRE = "fibre"
+    FRUITS = "fruits"
+    GUARDIAN_CORES = "guardian_cores"
+    HARD_ROCK = "hard_rock"
+    IRON = "iron"
     KNOWLEDGE = "knowledge"
+    LIGHT_METALS = "light_metals"
+    LIQUID_AETHER = "liquid_aether"
+    MEAT = "meat"
+    MEDICINAL_PLANTS = "medicinal_plants"
+    RARE_METALS = "rare_metals"
+    SEEDS = "seeds"
+    SOFT_ROCK = "soft_rock"
+    STONE = "stone"
+    WATER = "water"
+    WOOD = "wood"
 
 
 SPECIALIZATIONS = [
     "Prospector",
-    "Scholar",
-    "Warder"
+    "Researcher",
+    "Adventurer",
+    "Scout",
+    "Warrior",
 ]
 
 SPECIALIZATION_DESC = {
-    "Prospector": "Prospector - gifted at identifying resource hotspots and extracting raw resources from world zones.",
-    "Scholar": "Scholar - seeks ancient knowledge as a resource.",
-    "Warder": "Warder - skilled combatants whose purpose is battle."
+    "Prospector": "Prospector - specialise in detecting resource hotspots during expeditions, as well as mining hotspots even within the dungeon. Best for resource extraction expeditions; crucial for turning fruitless expeditions to fruitful ones. Not suited for battle though they can offer such service. Produced by a certain guardian type.",
+    "Researcher": "Researcher - Specialise in gathering knowledge in zones during expeditions. Crucial to bringing knowledge back to the dungeon, as well as performing high-level research on the knowledge that is brought back to create recipes for crafting. Best for knowledge-focused expeditions. Produced by a certain guardian type. Not suited for battle.",
+    "Warrior": "Warrior - specialise in combat and especially strong against all manner of dungeon enemies from beast and creatures all the way to humans and human-organised attacks. Crucial for safety of expedition parties, and best for expeditions that require killing certain creatures or wiping out certain enemies. They have exceptionally low resource gathering, planning  and logistics capabilities. Full capabilities are drawn under Adventurer types. Produced by a certain guardian type.",
+    "Scout": "best for scouting zones and providing information on potential risks in these zones, as well as rewards. Crucial for long-term planning and zone discovery. Produced by a certain guardian type.",
+    "Adventurer": "heroes with excellent planning and logistics skills, who are specialised at leading expeditions and controlling other units during expeditions. They are capable of handling expeditions with hyper-specific goals. They are naturally all-rounders, with a tiny bit of skill in every field. Produced by a specific guardian type.",
+
+
 }
